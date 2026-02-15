02f07af3f5a5de0769e586b14ed8e07ba383c7d2
WarmedxMints <warmedxmints@outlook.com>
2026-02-15 03:54:39 +0000

    Made notifications transparent to the mouse pointer

--- ODExplorer/App.xaml.cs
+++ ODExplorer/App.xaml.cs
@@ -21,7 +21,7 @@ namespace ODExplorer
     {
         public static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
-        public static readonly Version AppVersion = new(2, 0, 11);
+        public static readonly Version AppVersion = new(2, 0, 12);
 #if INSTALL || DEBUG
         public readonly static string BaseDirectory = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "OD Explorer");
 #else
